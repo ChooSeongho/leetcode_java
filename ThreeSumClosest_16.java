ecfc6e0bda0b62106bf0352a330ee20e0ef9b252
seongho.choo <csh9895@naver.com>
2025-01-09 22:52:21 +0900

    20250109_leetcode

---
+++ ThreeSumClosest_16.java
@@ -0,0 +1,51 @@
+import java.util.Arrays;
+
+public class ThreeSumClosest_16 {
+
+	public static void main(String[] args) {
+		int[][] input = {
+				{-1,2,1,-4},
+				{0,0,0}
+		};
+		int[] target = {1, 1};
+		
+		for(int i = 0; i < input.length; i++) {
+			System.out.println(threeSumClosest(input[i], target[i]));
+			System.out.println("---------------");
+			System.out.println();
+		}
+	}
+	
+	public static int threeSumClosest(int[] nums, int target) {
+		Arrays.sort(nums);
+		
+		int res = nums[0] + nums[1] + nums[nums.length-1];
+    	
+    	for(int i = 0; i < nums.length; i++) {
+    		int left = i + 1;
+    		int right = nums.length - 1;
+
+    		while(left < right) {
+    			int temp_res = nums[left] + nums[right] + nums[i];
+    			System.out.println("res = " + res);
+    			System.out.println("i = " + i + " left = " + left + " right = " + right);
+    			System.out.println("temp_res = " + temp_res);
+    			System.out.println("target-res = " + (target-res));
+    			if (temp_res < target) {
+    				left++;
+    			} else if (temp_res > target) {
+    				right--;
+    			} else {
+    				return temp_res;
+    			}
+    			
+    			if(Math.abs(target - temp_res) < Math.abs(target - res)) {
+    				res = temp_res;
+    			}
+    			System.out.println("---------------");
+    	      }
+    	}
+    	return res;
+    }
+
+}
